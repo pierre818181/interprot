dfa693de4c4fed22ea862ebaf64f0a403ec6f4b4
Liam <liambai2000@gmail.com>
2024-11-30 14:57:26 -0500

    Update some training params

--- interprot/training.py
+++ interprot/training.py
@@ -14,22 +14,23 @@ parser.add_argument("--data-dir", type=str, default="data/uniref50_1M_1022.parqu
 parser.add_argument("--esm2-weight", type=str, default="weights/esm2_t33_650M_UR50D.pt")
 parser.add_argument("-l", "--layer-to_use", type=int, default=24)
 parser.add_argument("--d-model", type=int, default=1280)
-parser.add_argument("--d-hidden", type=int, default=32768)
-parser.add_argument("-b", "--batch-size", type=int, default=4)
+parser.add_argument("--d-hidden", type=int, default=16384)
+parser.add_argument("-b", "--batch-size", type=int, default=48)
 parser.add_argument("--lr", type=float, default=2e-4)
 parser.add_argument("--k", type=int, default=128)
 parser.add_argument("--auxk", type=int, default=256)
 parser.add_argument("--dead-steps-threshold", type=int, default=2000)
-parser.add_argument("-e", "--max-epochs", type=int, default=4)
+parser.add_argument("-e", "--max-epochs", type=int, default=1)
 parser.add_argument("-d", "--num-devices", type=int, default=1)
+parser.add_argument("--model-suffix", type=str, default="")
 
 args = parser.parse_args()
-args.output_dir = f"results_l{args.layer_to_use}_dim{args.d_hidden}_k{args.k}"
+args.output_dir = f"results_l{args.layer_to_use}_dim{args.d_hidden}_k{args.k}_auxk{args.auxk}_{args.model_suffix}"
 
 if not os.path.exists(args.output_dir):
     os.mkdir(args.output_dir)
 
-sae_name = f"esm2_plm1280_l{args.layer_to_use}_sae{args.d_hidden}_k{args.k}_auxk{args.auxk}"
+sae_name = f"esm2_plm1280_l{args.layer_to_use}_sae{args.d_hidden}_k{args.k}_auxk{args.auxk}_{args.model_suffix}"
 wandb_logger = WandbLogger(
     project="interpretability",
     name=sae_name,
