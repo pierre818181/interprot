a4119d7a60a7b20ad60aaab9fddd3384b2bc56c2
Liam <liambai2000@gmail.com>
2024-11-30 21:44:22 -0500

    Set num_workers for data loader

--- interprot/data_module.py
+++ interprot/data_module.py
@@ -3,6 +3,7 @@ import pytorch_lightning as pl
 import torch
 from torch.utils.data import Dataset
 from utils import train_val_test_split
+import multiprocessing
 
 
 class PolarsDataset(Dataset):
@@ -19,10 +20,11 @@ class PolarsDataset(Dataset):
 
 # Data Module
 class SequenceDataModule(pl.LightningDataModule):
-    def __init__(self, data_path, batch_size):
+    def __init__(self, data_path, batch_size, num_workers=None):
         super().__init__()
         self.data_path = data_path
         self.batch_size = batch_size
+        self.num_workers = num_workers if num_workers is not None else multiprocessing.cpu_count() - 1
 
     def setup(self, stage=None):
         df = pr.read_parquet(self.data_path)
@@ -30,13 +32,22 @@ class SequenceDataModule(pl.LightningDataModule):
 
     def train_dataloader(self):
         return torch.utils.data.DataLoader(
-            PolarsDataset(self.train_data), batch_size=self.batch_size, shuffle=True
+            PolarsDataset(self.train_data),
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers
         )
 
     def val_dataloader(self):
-        return torch.utils.data.DataLoader(PolarsDataset(self.val_data), batch_size=self.batch_size)
+        return torch.utils.data.DataLoader(
+            PolarsDataset(self.val_data),
+            batch_size=self.batch_size,
+            num_workers=self.num_workers
+        )
 
     def test_dataloader(self):
         return torch.utils.data.DataLoader(
-            PolarsDataset(self.test_data), batch_size=self.batch_size
+            PolarsDataset(self.test_data),
+            batch_size=self.batch_size,
+            num_workers=self.num_workers
         )
